3270d1a3b9350d741f689df26eb0311e4ff13663
tbharber <tbharber@yahoo.com>
2024-05-19 22:36:59 -0400

    another iris update

--- iris/iris-2.py
+++ iris/iris-2.py
@@ -16,8 +16,32 @@ print ("iris target_names:\n", iris.target_names)
 X = iris.data
 y = iris.target
 
+print (X.shape)
+print (y.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+
+print (knn)
+
+knn.fit(X, y)
+
+#knn.predict([3, 5, 4, 2])
+
+X_new = [[3, 5, 4, 2], [5, 4, 3, 2]]
+print (knn.predict(X_new))
 
 
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X, y)
+print (knn.predict(X_new))
+
+from sklearn.linear_model import LogisticRegression 
+logreg = LogisticRegression()
+
+logreg.fit(X, y)
+
+print(logreg.predict(X_new))
 
 
 
